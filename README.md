d3e8fc9ae5313a45d2ce49afd2c2330d2dd9c4e3
Alexey Golubev <agolubev@mirantis.com>
2016-07-08 15:35:51 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# check_failed_jobs
+## check_failed_builds.py
+
+A very simple script to get list of Jenkins job builds, failed since last script execution
+
+## Usage
+
+./check_failed_builds.py job_list_file
+
+## Job list file syntax
+
+For the first run you can simply fill a file with a list of job URLs:
+https://jenkins.host.com/job/job_name_1/
+https://jenkins.host.com/job/job_name_2/
+
---
+++ check_failed_builds.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import json 
+import sys
+import urllib2
+import os.path
+
+if len(sys.argv) < 2:
+    print "Usage:", sys.argv[0], "filename"
+    exit(1)
+
+failed_number = 0
+job_dict = {} 
+failed_dict = {}
+jobs_file = sys.argv[1]
+
+if not os.path.isfile(jobs_file) and os.access(jobs_file, os.RW_OK):
+    print "Job list file doesn't exist or can't be opened."
+    exit(1)
+
+with open(jobs_file) as f:
+    for line in filter(None, (line.rstrip() for line in f)):
+       try:
+           url, bldno = (line.strip('\n').split(","))
+       except ValueError, e:
+           url, bldno = (line.strip('\n'),-1)
+
+       job_dict[url] = bldno
+       try:
+           jenkinsStream = urllib2.urlopen( url + "/lastFailedBuild/api/json" )
+       except urllib2.HTTPError, e:
+           print url + "/lastFailedBuild/api/json:", e, ". No failed builds?"
+           continue
+       try:
+           buildStatusJson = json.load( jenkinsStream )
+       except Exception:
+           continue
+        
+       if buildStatusJson["number"] > int(job_dict[url]):
+           if job_dict[url] != '-1':
+               failed_number += 1
+               failed_dict[url] = buildStatusJson["number"]
+       job_dict[url] = buildStatusJson["number"]
+            
+if failed_number > 0:
+    print "You have " + str(failed_number) + " failed jobs since last run.\nLatest failed builds: "
+    for k,v in failed_dict.items():
+        print " ",k + "/" + str(v)
+
+with open(jobs_file, "w") as out_file:
+    for k,v in job_dict.items():
+        out_file.write(k + "," + str(v) + '\n')
+
+sys.exit(0)
